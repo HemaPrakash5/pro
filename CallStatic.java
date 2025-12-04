87d767dfc71e7796300d6ffc2952be27877005ce
HemaPrakash5 <neelihemaprakash@gmail.com>
2025-12-04 21:13:46 +0530

    Add files via upload

---
+++ CallStatic.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class CallStatic
+{
+    static void Test()
+    {
+        System.out.println("Hello");
+    }
+    public static void main(String args[])
+    {
+        CallStatic.Test();
+    }
+}
